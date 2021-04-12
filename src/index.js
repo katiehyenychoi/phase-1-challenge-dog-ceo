4b59c2cbcbd942b5ddbf39ac3aed499976935e16
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-12 14:22:51 -0400

    Halfway done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('%c HI', 'color: firebrick')
+
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    let dogDiv = document.querySelector("#dog-image-container");
+    let dogUl = document.querySelector("div")
+
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then((dogResp) => {
+            console.log(dogResp);
+            let dogImg = document.createElement("img")
+            dogImg.src = dogResp.message[0]
+
+            dogDiv.append(dogImg)
+
+
+
+
+        })
+
+})
+
+

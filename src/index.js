8175321d6f20f27e44db76c9e2f573f7872e8c73
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-12 16:31:15 -0400

    Challenge revised

--- src/index.js
+++ src/index.js
@@ -2,24 +2,38 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log('%c HI', 'color: firebrick')
 
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
 
     let dogDiv = document.querySelector("#dog-image-container");
-    let dogUl = document.querySelector("div")
-
+    let breedsUl = document.querySelector("#dog-breeds");
     fetch(imgUrl)
         .then(res => res.json())
         .then((dogResp) => {
-            console.log(dogResp);
-            let dogImg = document.createElement("img")
-            dogImg.src = dogResp.message[0]
-
-            dogDiv.append(dogImg)
-
-
-
-
+            // console.log(dogResp);
+            dogResp.message.forEach((item) => {
+                let dogImg = document.createElement("img")
+                dogImg.src = item;
+                dogDiv.append(dogImg)
+            })
         })
 
+    fetch(breedUrl)
+        .then(res => res.json())
+        .then((resp) => {
+            // console.log(Object.keys(resp.message));
+            Object.keys(resp.message).forEach((breedName) => {
+                let blankLi = document.createElement("li")
+                blankLi.append(breedName)
+                breedsUl.append(blankLi)
+            })
+        })
+
+
+
+
 })
+
+
+
 
 

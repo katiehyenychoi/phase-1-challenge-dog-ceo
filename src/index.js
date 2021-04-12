d5af3cbff31cb72071a6ecf8e6ffe9a3fc3ab909
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-12 16:43:17 -0400

    Final Challenge 3

--- src/index.js
+++ src/index.js
@@ -2,38 +2,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log('%c HI', 'color: firebrick')
 
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    const breedUrl = "https://dog.ceo/api/breeds/list/all"
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
     let dogDiv = document.querySelector("#dog-image-container");
-    let breedsUl = document.querySelector("#dog-breeds");
+    let breedUl = document.querySelector("#dog-breeds")
+    
+     
     fetch(imgUrl)
         .then(res => res.json())
         .then((dogResp) => {
-            // console.log(dogResp);
-            dogResp.message.forEach((item) => {
+            console.log(dogResp);
+        
+            dogResp.message.forEach((Image) => {
+            
                 let dogImg = document.createElement("img")
-                dogImg.src = item;
-                dogDiv.append(dogImg)
+            dogImg.src = Image 
+            dogDiv.append(dogImg)
+               
             })
+        
         })
-
     fetch(breedUrl)
-        .then(res => res.json())
-        .then((resp) => {
-            // console.log(Object.keys(resp.message));
-            Object.keys(resp.message).forEach((breedName) => {
-                let blankLi = document.createElement("li")
-                blankLi.append(breedName)
-                breedsUl.append(blankLi)
-            })
+    .then(res => res.json())
+    .then((response) => {
+       // console.log(response)
+        Object.keys(response.message).forEach((breedName) => {
+        let blankLi = document.createElement("li")
+
+        blankLi.append(breedName)
+        breedUl.append(blankLi)
+        blankLi.addEventListener("click", ()=> {
+            blankLi.style.color = "red"
         })
+    })
 
-
-
-
+    })
 })
-
-
-
 
 

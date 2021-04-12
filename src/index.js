f4ae27de9b002497362056d939db46547fa1bb91
Jede <brahmajede@gmail.com>
2021-04-12 16:29:58 -0400

    challenge3

--- src/index.js
+++ src/index.js
@@ -2,24 +2,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log('%c HI', 'color: firebrick')
 
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
     let dogDiv = document.querySelector("#dog-image-container");
-    let dogUl = document.querySelector("div")
-
+    let breedUl = document.querySelector("#dog-breeds")
+    
+     
     fetch(imgUrl)
         .then(res => res.json())
         .then((dogResp) => {
             console.log(dogResp);
-            let dogImg = document.createElement("img")
-            dogImg.src = dogResp.message[0]
-
+        
+            dogResp.message.forEach((Image) => {
+            
+                let dogImg = document.createElement("img")
+            dogImg.src = Image 
             dogDiv.append(dogImg)
-
-
-
-
+               
+            })
+        
         })
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then((response) => {
+       // console.log(response)
+        Object.keys(response.message).forEach((breedName) => {
+        let blankLi = document.createElement("li")
 
+        blankLi.append(breedName)
+        breedUl.append(blankLi)
+        blankLi.addEventListener("click", ()=> {
+            blankLi.style.color = "red"
+        })
+    })
+
+    })
 })
 
 

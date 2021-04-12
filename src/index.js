da8b0461821589154ba2059ad55b0cb0a4171e42
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-12 17:30:54 -0400

    Challenge 4 done

--- src/index.js
+++ src/index.js
@@ -6,37 +6,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     let dogDiv = document.querySelector("#dog-image-container");
     let breedUl = document.querySelector("#dog-breeds")
-    
-     
+    let dropdown = document.querySelector("#breed-dropdown")
+
     fetch(imgUrl)
         .then(res => res.json())
         .then((dogResp) => {
-            console.log(dogResp);
-        
+            // console.log(dogResp);
+
             dogResp.message.forEach((Image) => {
-            
+
                 let dogImg = document.createElement("img")
-            dogImg.src = Image 
-            dogDiv.append(dogImg)
-               
+                dogImg.src = Image
+                dogDiv.append(dogImg)
+
             })
-        
+
         })
     fetch(breedUrl)
-    .then(res => res.json())
-    .then((response) => {
-       // console.log(response)
-        Object.keys(response.message).forEach((breedName) => {
-        let blankLi = document.createElement("li")
+        .then(res => res.json())
+        .then((response) => {
+            // console.log(response)
+            Object.keys(response.message).forEach((breedName) => {
+                let blankLi = document.createElement("li")
 
-        blankLi.append(breedName)
-        breedUl.append(blankLi)
-        blankLi.addEventListener("click", ()=> {
-            blankLi.style.color = "red"
+                blankLi.append(breedName)
+                breedUl.append(blankLi)
+                blankLi.addEventListener("click", () => {
+                    blankLi.style.color = "red"
+                })
+            })
+
         })
+
+    dropdown.addEventListener("click", (e) => {
+        fetch(breedUrl)
+            .then(res => res.json())
+            .then((response) => {
+                let dogBreedsArr = Object.keys(response.message);
+
+                let filteredArray = dogBreedsArr.filter(breed => {
+                    return breed.startsWith(e.target.value)
+                })
+                breedUl.innerText = ""
+
+                filteredArray.forEach((breed) => {
+                    makeLi(breed)
+                })
+
+            }
+
+
+
+        // console.log(e)
+        // if (e.target.value === "a") {
+
+        // console.log(filter((names) => names.startsWith("a")))
+
+        // console.log(startsWithA);
+        // }
     })
 
-    })
+    // const startsWithN = countries.filter((country) => country.startsWith("N"));
+
+
+
+
+
+
 })
 
 
